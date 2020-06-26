0234184c74d5c27a01e8633790a6f6e72a3841b4
Sam Keathley <samncollette@gmail.com>
2020-06-26 14:48:21 -0700

    added tomtom api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles.css';
 
 function App() {
+
+  const script = document.createElement('script');
+  const API_KEY = 'process.env.REACT_APP_TOMTOM_API_KEY';
+
+  useEffect(() => {
+    script.src = process.env.PUBLIC_URL + '/sdk/tomtom.min.js';
+    document.body.appendChild(script);
+    script.async = true;
+    script.onload = function () {
+      window.tomtom.L.map('map', {
+        source: 'vector',
+        key: API_KEY,
+        center: [37.769167, -122.478468],
+        basePath: '/sdk',
+        zoom: 15
+      });
+    }
+  }, [script]);
+
   return (
-    <div>
-      <h1>Howdy.</h1>
-    </div>
-  );
+    <div id='map'></div>
+  )
 }
 
 export default App;
