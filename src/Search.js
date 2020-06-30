b39bc36cf7fd95f2322d7875d42c4c5d93355edd
Sam Keathley <samncollette@gmail.com>
2020-06-29 19:06:08 -0700

    set up fetch with hidden key

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 function Search() {
 
+    useEffect(() => {
+        fetch("https://api.tomtom.com/search/2/search/beaverton.json?key=" + process.env.REACT_APP_TOMTOM_KEY)
+            .then(res => res.json())
+            .then(
+                data => console.log(data)
+            )
+    }, [])
+
     return (
         <div>
         </div>
