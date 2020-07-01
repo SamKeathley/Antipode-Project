8eff7e69d00c6450e0ac422571229de4e94faa13
Sam Keathley <samncollette@gmail.com>
2020-06-30 17:13:13 -0700

    figured out how to get lat and lon independently

--- src/Search.js
+++ src/Search.js
@@ -2,16 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 function Search() {
 
+    const [points, setPoints] = useState(null);
+
     useEffect(() => {
         fetch("https://api.tomtom.com/search/2/search/beaverton.json?key=" + process.env.REACT_APP_TOMTOM_KEY)
             .then(res => res.json())
             .then(
-                data => console.log(data)
-            )
+                res => {
+                    console.log(res.results[0])
+                    setPoints(res.results[0])
+                })
     }, [])
 
     return (
         <div>
+            {points && <div>{points.position.lat}</div>}
+            {points && <div>{points.position.lon}</div>}
         </div>
     );
 }
